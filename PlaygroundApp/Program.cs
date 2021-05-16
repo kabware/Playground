4d2fad00189c842db4bef4fdfa06764762b484d4
Kevin Brandon <5228717+kabware@users.noreply.github.com>
2021-05-16 19:00:14 +0100

    same again

--- PlaygroundApp/Program.cs
+++ PlaygroundApp/Program.cs
@@ -8,7 +8,14 @@ namespace PlaygroundApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Just before hell world!");
             Console.WriteLine("Hello World!");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Hell world! In a loop");
+                 i+= 2;
+            }
         }
     }
 }
